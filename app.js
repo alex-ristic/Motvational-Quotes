d4e06bfb084cf285993459849911787d0e5d4618
alex-ristic <alexristic0107@gmail.com>
2024-11-09 15:03:23 +0100

    made it interactive
    Made it into a funny quote generator

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 /* Variables */
 
+// DOM variables
 let btn = document.querySelector (".button")
 let quote = document.querySelector (".quote")
 let person = document.querySelector (".person")
-console.log(btn)
+let quoteStyles = document.querySelector(".quote-styles")
+let note = document.querySelector(".note")
+let tremble = 0
 
-let allQuotes = [
+// Variables - Quotes
+let motivationQuotes = [
     {
+        person: "Alex Hormozi",
+        quote: "\"Just Win\""
+    }, {
         person: "Jocko Willink",
         quote: "\"Discipline equals freedom.\""
     }, {
@@ -111,11 +118,215 @@ let allQuotes = [
     }
 ];
 
+let wisdomQuotes = [
+    {
+        person: "Socrates",
+        quote: "\"The only true wisdom is in knowing you know nothing.\""
+    }, {
+        person: "Lao Tzu",
+        quote: "\"He who knows others is wise; he who knows himself is enlightened.\""
+    }, {
+        person: "Albert Einstein",
+        quote: "\"A clever person solves a problem. A wise person avoids it.\""
+    }, {
+        person: "Rumi",
+        quote: "\"Yesterday I was clever, so I wanted to change the world. Today I am wise, so I am changing myself.\""
+    }, {
+        person: "Confucius",
+        quote: "\"The man who moves a mountain begins by carrying away small stones.\""
+    }, {
+        person: "Leonardo da Vinci",
+        quote: "\"Simplicity is the ultimate sophistication.\""
+    }, {
+        person: "Khalil Gibran",
+        quote: "\"Out of suffering have emerged the strongest souls; the most massive characters are seared with scars.\""
+    }, {
+        person: "Carl Jung",
+        quote: "\"Who looks outside, dreams; who looks inside, awakes.\""
+    }, {
+        person: "Thich Nhat Hanh",
+        quote: "\"People have a hard time letting go of their suffering. Out of a fear of the unknown, they prefer suffering that is familiar.\""
+    }, {
+        person: "Alan Watts",
+        quote: "\"You are under no obligation to be the same person you were five minutes ago.\""
+    }, {
+        person: "The Buddha",
+        quote: "\"In the end, only three things matter: how much you loved, how gently you lived, and how gracefully you let go of things not meant for you.\""
+    }, {
+        person: "Fyodor Dostoevsky",
+        quote: "\"The soul is healed by being with children.\""
+    }, {
+        person: "Henry David Thoreau",
+        quote: "\"It’s not what you look at that matters; it’s what you see.\""
+    }, {
+        person: "Seneca",
+        quote: "\"As is a tale, so is life: not how long it is, but how good it is, is what matters.\""
+    }, {
+        person: "Alan Watts",
+        quote: "\"Muddy water is best cleared by leaving it alone.\""
+    }, {
+        person: "Ralph Waldo Emerson",
+        quote: "\"What lies behind us and what lies before us are tiny matters compared to what lies within us.\""
+    }, {
+        person: "Victor Frankl",
+        quote: "\"When we are no longer able to change a situation, we are challenged to change ourselves.\""
+    }, {
+        person: "Marcus Aurelius",
+        quote: "\"You could leave life right now. Let that determine what you do and say and think.\""
+    }, {
+        person: "Anaïs Nin",
+        quote: "\"We don’t see things as they are, we see them as we are.\""
+    }, {
+        person: "Rumi",
+        quote: "\"The wound is the place where the Light enters you.\""
+    }
+];
+
+// Add event listener to the parent
+quoteStyles.addEventListener ("click", (e) => {
+    // Checks if a button is clicked
+    if (e.target.matches("button")){
+        // Checks if the clicked button has "style-active" (if it is already active)
+        if (e.target.classList.contains("style-active")) {
+            // if it has "style-active"
+            e.target.classList.remove ("style-active")
+
+            // funny tremble
+            if (tremble === 0) {
+                note.innerText = `Good. Now click "New Quote" to see me trembleee...!"`
+            } else if (tremble === 1 || tremble === 1.1 || tremble === 1.2 || tremble === 1.3) {
+                note.innerText = "You're not following the instructions... Choose one!"
+            }
+        } else {
+            //if it doesn't have "style-active"
+
+            // Checks if any other button in the parent element has "style-active" and if it has, removes it
+            const activeBtn = e.target.parentElement.querySelector(".style-active")
+            if (activeBtn) {
+                activeBtn.classList.remove("style-active")
+            }
+            
+            // Adds "style-active" to the button clicked
+            e.target.classList.add ("style-active")
+
+            // funny tremble 
+            if (tremble === 0) {
+                note.innerText = `You're not listening to me... Deselect "${e.target.innerText}"`
+            } else if (tremble === 1 || tremble === 1.1 || tremble === 1.2 || tremble === 1.3) {
+                note.innerText = "Good job! Now click \"New Quote\""
+            }
+        }
+    }
+})
+
 
 btn.addEventListener ("click", () => {
-    let random = Math.floor(allQuotes.length * Math.random());
-    console.log(allQuotes[random].person)
+    // Determine which button is active (has style-act)
+    const activeBtn = document.querySelector(".style-active")
 
-    person.innerText = allQuotes[random].person;
-    quote.innerText = allQuotes[random].quote;
+    /* If no button has "style-active" (it is null) - text trembles to showcase that they need to select motivation or wisdom ; 
+    if it is not null, then just display a new quote.
+    This if is just for fun. You can delete it. */
+    if (activeBtn === null) {
+
+        //funny tremble
+        if (tremble === 0) {
+
+            note.classList.add ("tremble")
+            setTimeout(() => {
+
+                note.classList.remove("tremble");
+                note.innerText = "I just trembled! You saw it?"
+
+                setTimeout (() => {
+                    note.innerText = "Awesome... Now select one."
+                    tremble = 1
+                }, 2500)
+
+            }, 1100);
+
+        } else if (tremble === 1) {
+
+            note.classList.add ("tremble")
+            setTimeout(() => {
+                note.classList.remove("tremble");
+                note.innerText = "Once was enough... Select one."
+                tremble = 1.1
+            }, 1100);
+
+        } else if (tremble === 1.1) {
+
+            note.classList.add ("tremble")
+            setTimeout(() => {
+                note.classList.remove("tremble");
+                note.innerText = "Please don't make me tremble anymore... Select one please."
+                tremble = 1.2
+            }, 1100);
+
+        } else if (tremble === 1.2) {
+
+            note.classList.add ("tremble")
+            setTimeout(() => {
+                note.classList.remove("tremble");
+                note.innerText = "That's it! I'm protesting! No more trembling from me!"
+                tremble = 1.3
+            }, 1100);
+
+        } else if (tremble === 1.3) {
+
+                note.innerText = "I told you I'm not trembling anymore!"
+
+        }
+            
+    } else {
+
+        // funny tremble. can delete the if.
+        if (tremble === 1 || tremble === 1.1 || tremble === 1.2 || tremble === 1.3 || tremble === 2) {
+            
+            // Determine which quotes to use based on which button is active
+            let useQuotes
+            if (activeBtn.innerText === "Motivation"){
+                useQuotes = motivationQuotes
+            } else if (activeBtn.innerText === "Wisdom"){
+                useQuotes = wisdomQuotes
+            } else {
+                useQuotes = null;
+            }
+            
+            // new quote appears
+            let random = Math.floor(useQuotes.length * Math.random());
+            person.innerText = useQuotes[random].person;
+            quote.innerText = useQuotes[random].quote;
+
+            // funny tremble - goodbye
+            if (tremble === 2) {
+
+            } else {
+
+                setTimeout(() => {
+                    
+                    note.innerText = "That's it from me my friend... I'm out."
+                    
+                    setTimeout (() => {
+                        note.innerText = " "
+                        tremble = 2
+                    }, 2500)
+                    
+                }, 1100);
+                
+            }
+            
+        } else {
+            setTimeout(() => {
+
+                note.innerText = `Please listen to me! Deselect "${activeBtn.innerText}."`
+
+            }, 200)
+        }
+    }
+
+    
 })
+
+
+   
